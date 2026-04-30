908d5144531ea35049b3eeb15c1d32cac3045934
yash-sojitra-20 <yash.sojitra.2020@gmail.com>
2026-04-30 15:10:54 +0530

    Added ktor-demo project and configured project-specific .gitignore

---
+++ ktor-demo/settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "ktor-demo"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
---
+++ ktor-demo/src/test/kotlin/ApplicationTest.kt
@@ -0,0 +1,36 @@
+import com.example.module
+import io.ktor.client.request.get
+import io.ktor.client.statement.bodyAsText
+import io.ktor.http.HttpStatusCode
+import io.ktor.server.testing.testApplication
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import io.ktor.http.contentType
+import kotlin.test.assertContains
+
+class ApplicationTest {
+
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            module()
+        }
+        val response = client.get("/")
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("Hello World!", response.bodyAsText())
+    }
+
+    @Test
+    fun testNewEndpoint() = testApplication {
+        application {
+            module()
+        }
+
+        val response = client.get("/test1")
+
+        assertEquals(HttpStatusCode.OK, response.status)
+        assertEquals("html", response.contentType()?.contentSubtype)
+        assertContains(response.bodyAsText(), "Hello From Ktor")
+    }
+}
