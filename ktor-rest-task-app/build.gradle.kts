1bcbb38a1216ba747fc7dbe596c94d385783b5a4
yash-sojitra-20 <yash.sojitra.2020@gmail.com>
2026-05-03 16:06:14 +0530

    Added ktor-rest-task-app project and modified project-specific .gitignore

---
+++ ktor-rest-task-app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    alias(libs.plugins.kotlin.jvm)
+    alias(ktorLibs.plugins.ktor)
+    alias(libs.plugins.kotlin.serialization)
+}
+
+group = "com.example"
+version = "1.0.0-SNAPSHOT"
+
+application {
+    mainClass = "io.ktor.server.netty.EngineMain"
+}
+
+kotlin {
+    jvmToolchain(21)
+}
+dependencies {
+    implementation(ktorLibs.serialization.kotlinx.json)
+    implementation(ktorLibs.server.config.yaml)
+    implementation(ktorLibs.server.contentNegotiation)
+    implementation(ktorLibs.server.core)
+    implementation(ktorLibs.server.netty)
+    implementation(ktorLibs.server.resources)
+    implementation(libs.logback.classic)
+
+    testImplementation(kotlin("test"))
+    testImplementation(ktorLibs.server.testHost)
+    testImplementation("com.jayway.jsonpath:json-path:2.9.0")
+
+    testImplementation(ktorLibs.client.contentNegotiation)
+}
